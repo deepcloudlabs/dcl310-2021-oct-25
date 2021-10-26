705f5d0192cc9a1e921ec06fe0fbc4b3f29a165e
deepcloudlabs <deepcloudlabs@gmail.com>
2021-10-26 12:33:37 +0300

    initial commit

--- mastermind-ko/js/mastermind.js
+++ mastermind-ko/js/mastermind.js
@@ -19,36 +19,37 @@ class MoveES6 {
 
 class GameStatistics {
     constructor() {
-        this.wins = 0;
-        this.loses = 0;
-        this.total = 0;
+        this.wins = ko.observable(0);
+        this.loses = ko.observable(0);
+        this.total = ko.computed(() => {
+            return this.wins() + this.loses()
+        });
     }
 
     playerWins = () => {
-        this.wins++;
-        this.total++;
+        this.wins(this.wins()+1);
     }
     playerLoses = () => {
-        this.loses++;
-        this.total++;
+        this.loses(this.loses()+1);
     }
 }
 
 //ES6: class
-class MastermindViewModel {
+class MastermindViewModel { // Knockout's ViewModel
     constructor() {
-        this.gameLevel = 3;
-        this.tries = 0;
-        this.moves = []; // empty array
+        this.gameLevel = ko.observable(3);
+        this.tries = ko.observable(0);
+        this.guess = ko.observable(123);
+        this.moves = ko.observableArray([]); // empty array
         this.secret = this.createSecret();
-        this.counter = 60;
+        this.counter = ko.observable(60);
         this.statistics = new GameStatistics();
         // this.play = this.play.bind(this);
     }
 
     countDown = () => {
-        this.counter--;
-        if (this.counter <= 0) {
+        this.counter(this.counter()-1);
+        if (this.counter() <= 0) {
             this.initGame();
             this.statistics.playerLoses();
             return "loses";
@@ -56,24 +57,24 @@ class MastermindViewModel {
         return "continues";
     }
 
-    play = (guess) => {
-        this.tries++;
-        if (Number(guess) === this.secret) {
-            this.gameLevel++;
-            if (this.gameLevel > 10) {
-                this.gameLevel = 3;
+    play = () => {
+        this.tries(this.tries()+1);
+        if (Number(this.guess()) === this.secret) {
+            this.gameLevel(this.gameLevel()+1);
+            if (this.gameLevel() > 10) {
+                this.gameLevel(3);
                 this.initGame();
                 this.statistics.playerWins();
                 return "wins";
             }
             this.initGame();
         } else {
-            if (this.tries > 10) {
+            if (this.tries() > 10) {
                 this.initGame();
                 this.statistics.playerLoses();
                 return "loses";
             } else {
-                this.moves.push(this.createMove(guess));
+                this.moves.push(this.createMove(this.guess()));
             }
         }
     }
@@ -81,7 +82,7 @@ class MastermindViewModel {
     createSecret = () => {
         let digits = []; // [5, 4, 9] -> 549
         digits.push(this.createRandomDigit(1, 9));
-        while (digits.length < this.gameLevel) {
+        while (digits.length < this.gameLevel()) {
             let randomDigit = this.createRandomDigit(0, 9);
             if (!digits.includes(randomDigit))
                 digits.push(randomDigit);
@@ -94,9 +95,9 @@ class MastermindViewModel {
     }
 
     initGame = () => {
-        this.tries = 0;
-        this.counter = 60;
-        this.moves = [];
+        this.tries(0);
+        this.counter(60);
+        this.moves([]);
         this.secret = this.createSecret();
     }
 
